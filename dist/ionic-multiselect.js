1ed4b8e8a57a7e5080479f3e7d200e91a2d162aa
mapeveri <martinpeveri@gmail.com>
2015-12-07 12:09:55 -0300

    New version 0.0.5 with jsDoc for comments and bug in filter with one item for translate

--- dist/ionic-multiselect.js
+++ dist/ionic-multiselect.js
@@ -2,190 +2,257 @@
 "use strict";
 
 angular.module("ionic-multiselect", [])
-.filter('translateItem', ["$injector", function($injector) {
-  return function(item, scope) {
+  .filter('translateItem', ["$injector", "$parse", function($injector, $parse) {
+    /**
+    * @name translateItem
+    * @desc Translate value item modal in base to value_property
+    * @param {Object} item: Object data item
+    * @returns {String}
+    */
+    return function(item, scope) {
 
-    var filterTranslate;
-    try{
-        var filterTranslate = $injector.get('$filter');
-        return filterTranslate('translate')(scope.indexTranslate + eval('item.' + scope.valueProperty));
-    }catch(e){
-      return "";
-    }
-  };
-}])
-//Filter that show data text
-.filter('showTextData', ["$injector", function($injector) {
-  return function(value, scope) {
-
-    var filterTranslate;
-    try{
-        var filterTranslate = $injector.get('$filter');
-    }catch(e){}
-
-    var arr = [];
-    if(value.indexOf("-") > -1){
-      arr = value.split("-");
-      arr = arr.filter(Boolean);
-      var tot = arr.length;
-      for(var i=0;i<tot;i++){
-          if(scope.isTranslate){
-            var valueAux = filterTranslate('translate')(scope.indexTranslate + arr[i].trim());
-          }else{
-            var valueAux = arr[i].trim();
-          }
-          arr[i] = valueAux;
+      var filterTranslate;
+      try{
+          var filterTranslate = $injector.get('$filter');
+          //Parse item property of the object
+          var parseFun = $parse('item.' + scope.valueProperty);
+          var valueTranslate = parseFun(scope);
+          return filterTranslate('translate')(scope.indexTranslate + valueTranslate);
+      }catch(e){
+        return "";
       }
-    }
-    if(arr.length == 0){
-      return "";
-    }else{
-      return arr;
-    }
-  };
-}])
-.directive("multiselect", ["$ionicModal", function($ionicModal) {
-  return {
-    restrict: "E",
-    //Template con el item para mostrar el modal y los resultados
-    template: function(element, attrs) {
-      if (attrs.templateUrl) {
-        return "<ng-include src=\"'" + attrs.templateUrl + "'\"></ng-include>";
-      } else {
-        return '';
-      }
-    },
-    //Atributos
-    scope: {
-      items: "=", // Needs to have a value
-      value: "=", // Needs to have a value
-      valueChangedCallback: "&valueChanged", // The callback used to signal that the value has changed
-      getCustomTextCallback: "&getCustomText" // The callback used to get custom text based on the selected value
-    },
-    //Conectado la directiva
-    link: function(scope, element, attrs) {
-      // Header usado en ion-header-bar
-      scope.headerText = attrs.headerText || '';
-      // Text displayed on label
-      scope.text = attrs.text || '';
-      scope.defaultText = attrs.text || '';
-      // Data binding properties
-      scope.checkedProperty = attrs.checkedProperty || "checked";
-      scope.textProperty = attrs.textProperty || "text";
-      scope.valueProperty = attrs.valueProperty || "id";
-      //Translate properties
-      scope.isTranslate = (attrs.isTranslate === "true") || false;
-      scope.indexTranslate = attrs.indexTranslate || "";
-      // The modal properties
-      scope.modalTemplateUrl = attrs.modalTemplateUrl;
-      scope.modalAnimation = attrs.modalAnimation;
-      // Note properties
-      scope.noteText = attrs.noteText || "";
+    };
+  }])
+  .filter('showTextData', ["$injector", function($injector) {
+    /**
+    * @name showTextData
+    * @desc Filter that show data text
+    * @param {String} value: Value text show
+    * @returns {Array}
+    */
+    return function(value, scope) {
 
-      //Inicializa el modal con los items
-      if (scope.modalTemplateUrl) {
-        $ionicModal.fromTemplateUrl(
-          scope.modalTemplateUrl,
-          {
-            scope: scope,
-            animation: scope.modalAnimation
-          }
-        ).then(function(modal) {
-          scope.modal = modal;
-        });
-      } else {
-        scope.modal = $ionicModal.fromTemplate('',
-          {
-            scope: scope,
-            animation: scope.modalAnimation
-          }
-        );
-      }
+      var filterTranslate;
+      try{
+          var filterTranslate = $injector.get('$filter');
+      }catch(e){}
 
-      //Cierro el modal al destruir la pantalla
-      scope.$on("$destroy", function() {
-        scope.modal.remove();
-      });
+      var arr = [];
+      if(value.indexOf("-") > -1){
+        arr = value.split("-");
+        arr = arr.filter(Boolean);
+        var tot = arr.length;
 
-      //Obtiene el texto de la propiedad de un objeto
-      scope.getItemText = function(item) {
-        return scope.textProperty ? item[scope.textProperty] : item;
-      };
-
-      //Obtiene el valor de la propiedad de un objeto
-      scope.getItemValue = function(item) {
-        return scope.valueProperty ? item[scope.valueProperty] : item;
-      };
-
-      //Obtiene los items seleccionados
-      scope.getText = function(value) {
-        // Push the values into a temporary array so that they can be iterated through
-        var temp = value ? value : [];
-
-        var text = "";
-        if (temp.length) {
-          //Concatena los items
-          angular.forEach(scope.items, function(item, key) {
-            for (var i = 0; i < temp.length; i++) {
-              if (scope.getItemValue(item) == temp[i]) {
-                //if is translate
-                if(scope.isTranslate){
-                  text += (text.length ? "-" : "") + scope.getItemValue(item);
-                }else{
-                  text += (text.length ? "-" : "") + scope.getItemText(item);
-                }
-                break;
-              }
+        for(var i=0;i<tot;i++){
+            if(scope.isTranslate){
+              var valueAux = filterTranslate('translate')(scope.indexTranslate + arr[i].trim());
+            }else{
+              var valueAux = arr[i].trim();
             }
+            arr[i] = valueAux;
+        }
+      }else{
+        arr.push(value);
+      }
+
+      if(arr.length == 0){
+        return "";
+      }else{
+        return arr;
+      }
+    };
+  }])
+  /**
+  * @desc Multiselect for Ionic Framework
+  * @example 
+  *   <multiselect
+  *      header-text="Header"
+  *      items="data"
+  *      text-property="value"
+  *      value-property="id"
+  *      is-translate="true"
+  *      index-translate="TAG_TRANSLATE"
+  *      text="Text default multiselect"
+  *      modal-template-url="bower_components/ionic-multiselect/templates/modal-template.html"
+  *      template-url="bower_components/ionic-multiselect/templates/item-template.html"
+  *      note-text="Note Text"
+  *      value-changed="onValueChanged(value)">
+  *   </multiselect>
+  */
+  .directive("multiselect", ["$ionicModal", function($ionicModal) {
+    return {
+      restrict: "E",
+      //Template with item for show modal
+      template: function(element, attrs) {
+        if (attrs.templateUrl) {
+          return "<ng-include src=\"'" + attrs.templateUrl + "'\"></ng-include>";
+        } else {
+          return '';
+        }
+      },
+      //Attributes
+      scope: {
+        items: "=", // Needs to have a value
+        value: "=", // Needs to have a value
+        valueChangedCallback: "&valueChanged", // The callback used to signal that the value has changed
+        getCustomTextCallback: "&getCustomText" // The callback used to get custom text based on the selected value
+      },
+      //Conected directive
+      link: function(scope, element, attrs) {
+        // Header usado en ion-header-bar
+        scope.headerText = attrs.headerText || '';
+        // Text displayed on label
+        scope.text = attrs.text || '';
+        scope.defaultText = attrs.text || '';
+        // Data binding properties
+        scope.checkedProperty = attrs.checkedProperty || "checked";
+        scope.textProperty = attrs.textProperty || "text";
+        scope.valueProperty = attrs.valueProperty || "id";
+        //Translate properties
+        scope.isTranslate = (attrs.isTranslate === "true") || false;
+        scope.indexTranslate = attrs.indexTranslate || "";
+        // The modal properties
+        scope.modalTemplateUrl = attrs.modalTemplateUrl;
+        scope.modalAnimation = attrs.modalAnimation;
+        // Note properties
+        scope.noteText = attrs.noteText || "";
+
+        //Init modal with the items
+        if (scope.modalTemplateUrl) {
+          $ionicModal.fromTemplateUrl(
+            scope.modalTemplateUrl,
+            {
+              scope: scope,
+              animation: scope.modalAnimation
+            }
+          ).then(function(modal) {
+            scope.modal = modal;
           });
         } else {
-          // Pone el texto por default
-          text = scope.defaultText;
-        }
-
-        // If a callback has been specified for the text
-        return scope.getCustomTextCallback({value: value}) || text;
-      };
-
-      //Oculta el modal
-      scope.hideItems = function(event) {
-        scope.modal.hide();
-      };
-
-      //Raised by watch when the value changes
-      scope.onValueChanged = function(newValue, oldValue) {
-        scope.text = scope.getText(newValue);
-
-        // Notify subscribers that the value has changed
-        scope.valueChangedCallback({value: scope.text});
-      };
-
-      //Muestro el modal
-      scope.showItems = function(event) {
-        event.preventDefault(); // Prevent the event from bubbling
-        scope.modal.show();
-      };
-
-      //Validates the current list
-      scope.validate = function(item) {
-        scope.value = [];
-        if (scope.items) {
-          angular.forEach(scope.items, function(item, key) {
-            if (item[scope.checkedProperty]) {
-              scope.value[key] = scope.getItemValue(item);
-            }else{
-              scope.value[key] = "";
+          scope.modal = $ionicModal.fromTemplate('',
+            {
+              scope: scope,
+              animation: scope.modalAnimation
             }
-          });
+          );
         }
-        scope.hideItems();
-      };
 
-      // Watch the value property, as this is used to build the text
-      scope.$watch(function(){
-        return scope.value;
-      }, scope.onValueChanged, true);
-    }
-  };
-}]);
+        //Remove modal
+        scope.$on("$destroy", function() {
+          scope.modal.remove();
+        });
+
+        /**
+        * @name getItemText
+        * @desc Get text property of objeto
+        * @param {Object} item: Object data item
+        * @returns {String}
+        */
+        scope.getItemText = function(item) {
+          return scope.textProperty ? item[scope.textProperty] : item;
+        };
+
+        /**
+        * @name getItemValue
+        * @desc Get value property of objet
+        * @param {Object} item: Object data item
+        * @returns {String}
+        */
+        scope.getItemValue = function(item) {
+          return scope.valueProperty ? item[scope.valueProperty] : item;
+        };
+
+        /**
+        * @name getText
+        * @desc Get items selected
+        * @param {Array} value: Array with values showed in modal
+        * @returns {String}
+        */
+        scope.getText = function(value) {
+          // Push the values into a temporary array so that they can be iterated through
+          var temp = value ? value : [];
+
+          var text = "";
+          if (temp.length) {
+            //Concatenate the items
+            angular.forEach(scope.items, function(item, key) {
+              for (var i = 0; i < temp.length; i++) {
+                if (scope.getItemValue(item) == temp[i]) {
+                  //if is translate
+                  if(scope.isTranslate){
+                    text += (text.length ? "-" : "") + scope.getItemValue(item);
+                  }else{
+                    text += (text.length ? "-" : "") + scope.getItemText(item);
+                  }
+                  break;
+                }
+              }
+            });
+          } else {
+            // Set text default
+            text = scope.defaultText;
+          }
+
+          // If a callback has been specified for the text
+          return scope.getCustomTextCallback({value: value}) || text;
+        };
+
+        /**
+        * @name hideItems 
+        * @desc Hide modal
+        * @param {Object} event: Event js
+        */
+        scope.hideItems = function(event) {
+          scope.modal.hide();
+        };
+
+        /**
+        * @name onValueChanged
+        * @desc Raised by watch when the value changes
+        * @param {Object} newValue: New value object
+        * @param {Object} oldValue: Old value object
+        */
+        scope.onValueChanged = function(newValue, oldValue) {
+          scope.text = scope.getText(newValue);
+
+          // Notify subscribers that the value has changed
+          scope.valueChangedCallback({value: scope.text});
+        };
+
+        /**
+        * @name showItems
+        * @desc Show modal
+        * @param {Object} event: Event js
+        */
+        scope.showItems = function(event) {
+          event.preventDefault(); // Prevent the event from bubbling
+          scope.modal.show();
+        };
+
+        /**
+        * @name validate
+        * @desc Validates the current list
+        * @param {Object} item: Object data item
+        */
+        scope.validate = function(item) {
+          scope.value = [];
+          if (scope.items) {
+            angular.forEach(scope.items, function(item, key) {
+              if (item[scope.checkedProperty]) {
+                scope.value[key] = scope.getItemValue(item);
+              }else{
+                scope.value[key] = "";
+              }
+            });
+          }
+          scope.hideItems();
+        };
+
+        // Watch the value property, as this is used to build the text
+        scope.$watch(function(){
+          return scope.value;
+        }, scope.onValueChanged, true);
+      }
+    };
+  }]);
 }());
